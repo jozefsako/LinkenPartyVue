f2ebfc9c51120f79f917e72d43afb1e648248ff1
GinkgoLou <loury.jacob@student.vinci.be>
2019-05-20 10:14:21 +0200

    google map adress

--- src/main.js
+++ src/main.js
@@ -2,12 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import Argon from '@/plugins/argon-kit'
 import router from './router'
+import { store } from './store/store'
+
 
 Vue.use(Argon);
+
 Vue.config.productionTip = false
+Vue.use(require('vue-resource').default)
 
 new Vue({
   el: '#app',
+  store,
   router,
   render: h => h(App)
 })
