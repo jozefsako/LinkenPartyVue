473932ab34dd696f0db7543fd933fbd81cf37c87
GinkgoLou <loury.jacob@student.vinci.be>
2019-05-21 16:40:01 +0200

    display allEvents

--- src/components/PartyList.vue
+++ src/components/PartyList.vue
@@ -2,13 +2,13 @@
   <div class="col-lg-12" style="padding: 0 !important;">
     <div class="row row-grid list-party">
       <div v-for="party in parties" :key="party.id" class="col-lg-4">
-        <div class="card card-lift--hover shadow border-0" style="margin-top: 30px;">
+        <div class="card card-lift--hover shadow border-0" style="margin-bottom: 30px;">
           <div class="card-body py-3">
             <div class="icon icon-shape icon-shape-primary rounded-circle mb-4">
               <i class="ni ni-sound-wave"></i>
             </div>
             <h6 class="text-primary text-uppercase">{{party.fields.name_event}}</h6>
-            <p class="description mt-3">{{party.fields.description_event}}</p>
+            <p class="description mt-3" style="max-height: 100px; min-height: 100px;">{{party.fields.description_event}}</p>
             <h5 class="description mt-3">{{party.fields.address_event}}</h5>
             <div>
               <span class="badge badge-pill badge-primary">{{party.fields.theme_event}}</span>
